9754e2bc2139cb66d8e51c2c9bdb8ca7736fa782
maliang <ma.liang.465032766@gmail.com>
2024-04-25 08:38:15 +0800

    first commit

---
+++ app/example/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="bg-gray-200 p-4 mt-10">
+            <h1>Example Page</h1>
+            <p>This is an example page.</p>
+        </div>
+    )
+}
